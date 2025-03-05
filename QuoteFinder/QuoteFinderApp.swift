1b9c9f3975442084fd1a3b7a365a2c8d059e0b2a
Kevin Chen <kchen@lcs.on.ca>
2025-03-04 20:50:38 -0500

    Initial Commit

---
+++ QuoteFinder/QuoteFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteFinderApp.swift
+//  QuoteFinder
+//
+//  Created by Haowen Chen on 2025-03-04.
+//
+
+import SwiftUI
+
+@main
+struct QuoteFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
